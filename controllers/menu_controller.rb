ceb2f851e9fb4bb3e36292f202f91f84c6ccf66a
Anthony Hart <cidsolutions@yahoo.com>
2015-12-28 21:55:42 -0800

    Checkpoint 23 Menu: completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -21,89 +21,175 @@ class MenuController
     puts "You picked #{selection}"
 
     case selection
-     when 1
-       system "clear"
-       view_all_entries
-       main_menu
-     when 2
-       system "clear"
-       create_entry
-       main_menu
-     when 3
-       system "clear"
-       search_entries
-       main_menu
-     when 4
-       system "clear"
-       read_csv
-       main_menu
-     when 5
-       puts "Good-bye!"
- # #8
-       exit(0)
- # #9
-     else
-       system "clear"
-       puts "Sorry, that is not a valid input"
-       main_menu
-     end
-   end
+    when 1
+      system "clear"
+      view_all_entries
+      main_menu
+    when 2
+      system "clear"
+      create_entry
+      main_menu
+    when 3
+      system "clear"
+      search_entries
+      main_menu
+    when 4
+      system "clear"
+      read_csv
+      main_menu
+    when 5
+      puts "Good-bye!"
+      # #8
+      exit(0)
+      # #9
+    else
+      system "clear"
+      puts "Sorry, that is not a valid input"
+      main_menu
+    end
+  end
 
- # #10
-   def view_all_entries
+  # #10
+  def view_all_entries
 
-     @address_book.entries.each do |entry|
-       system "clear"
-       puts entry.to_s
+    @address_book.entries.each do |entry|
+      system "clear"
+      puts entry.to_s
 
-       entry_submenu(entry)
-     end
+      entry_submenu(entry)
+    end
 
-     system "clear"
-     puts "End of entries"
-   end
+    system "clear"
+    puts "End of entries"
+  end
 
-   def create_entry
-     system "clear"
-     puts "New AddressBloc Entry"
+  def create_entry
+    system "clear"
+    puts "New AddressBloc Entry"
 
-     print "Name: "
+    print "Name: "
+    name = gets.chomp
+    print "Phone Number"
+    phone = gets.chomp
+    print "Email:"
+    email = gets.chomp
+
+    @address_book.add_entry(name, phone, email)
+
+    system "clear"
+    puts "New entry created"
+  end
+
+  def search_entries
+    print "Search by name: "
      name = gets.chomp
-     print "Phone Number"
-     phone = gets.chomp
-     print "Email:"
-     email = gets.chomp
 
-     @address_book.add_entry(name, phone, email)
-
+     match = @address_book.binary_search(name)
      system "clear"
-     puts "New entry created"
-   end
 
-   def search_entries
-   end
+     if match
+       puts match.to_s
+       search_submenu(match)
+     else
+       puts "No match found for #{name}"
+     end
+  end
 
-   def read_csv
-   end
+  def read_csv
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+      entry_count = @address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
+  end
 end
-   def entry_submenu(entry)
 
-     puts "n - next entry"
-     puts "d - delete entry"
-     puts "e - edit entry"
+def entry_submenu(entry)
+
+  puts "n - next entry"
+  puts "d - delete entry"
+  puts "e - edit entry"
+  puts "m - return to main menu"
+
+  selection = $stdin.gets.chomp
+
+  case selection
+  when "n"
+  when "d"
+    delete_entry(entry)
+  when "e"
+    edit_entry(entry)
+    entry_submenu(entry)
+  when "m"
+    system "clear"
+    main_menu
+  else
+    system "clear"
+    puts "#{selection} is not a valid input"
+    entries_submenu(entry)
+  end
+end
+
+def delete_entry(entry)
+  @address_book.entries.delete(entry)
+  puts "#{entry.name} has been deleted"
+end
+
+def edit_entry(entry)
+
+  print "Updated name: "
+  name = gets.chomp
+  print "Updated phone number: "
+  phone_number = gets.chomp
+  print "Updated email: "
+  email = gets.chomp
+
+  entry.name = name if !name.empty?
+  entry.phone_number = phone_number if !phone_number.empty?
+  entry.email = email if !email.empty?
+  system "clear"
+
+  puts "Updated entry:"
+  puts entry
+end
+
+def search_submenu(entry)
+
+     puts "\nd - delete entry"
+     puts "e - edit this entry"
      puts "m - return to main menu"
+ 
      selection = gets.chomp
-     case selection
-     when "n"
 
+
+     case selection
      when "d"
+       system "clear"
+       delete_entry(entry)
+       main_menu
      when "e"
+       edit_entry(entry)
+       system "clear"
+       main_menu
      when "m"
        system "clear"
        main_menu
      else
        system "clear"
        puts "#{selection} is not a valid input"
-       entries_submenu(entry)
+       puts entry.to_s
+       search_submenu(entry)
      end
    end
